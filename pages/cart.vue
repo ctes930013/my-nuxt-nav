14ee4d2bbf27d20bc4f2b95db8f3325a0827400a
Ling Hsuan <lingxuank86@gmail.com>
2025-07-30 16:20:50 +0800

    feat:新增沒有購物車佈局

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,26 +1,32 @@
 <template>
   <div>
     <v-container class="my-5 v-container-padding">
-      <v-row>
-        <v-col cols="12" v-for="(product, index) in productList">
-          <v-row class="g-3">
-            <v-col cols="8" class="pa-0">
-              <div class="d-flex custom-align-center">
-                <img :src="product.image" height="100px" alt="桌機圖">
-                <h3 class="fw-bold mx-4 mb-0">{{ product.name }}</h3>
-              </div>
-            </v-col>
-            <v-col cols="4" class="d-flex custom-align-center pa-0" style="justify-content: right">
-              <div class="d-flex custom-align-center mr-2">
-                <i class="bi bi-dash-square" @click="minus(product.id)"></i>
-                <h4 class="mx-3 text-center mb-0">{{ product.quantity }}</h4>
-                <i class="bi bi-plus-square" @click="add(product.id)"></i>
-              </div>
-            </v-col>
-            <v-divider v-if="index < productList.length - 1" class="my-2" />
-          </v-row>
-        </v-col>
-      </v-row>
+      <div v-if="productList.length > 0">
+        <v-row>
+          <v-col cols="12" v-for="(product, index) in productList">
+            <v-row class="g-3">
+              <v-col cols="8" class="pa-0">
+                <div class="d-flex custom-align-center">
+                  <img :src="product.image" height="100px" alt="桌機圖">
+                  <h3 class="fw-bold mx-4 mb-0">{{ product.name }}</h3>
+                </div>
+                </v-col>
+                <v-col cols="4" class="d-flex custom-align-center pa-0" style="justify-content: right">
+                <div class="d-flex custom-align-center mr-2">
+                  <i class="bi bi-dash-square" @click="minus(product.id)"></i>
+                  <h4 class="mx-3 text-center mb-0">{{ product.quantity }}</h4>
+                  <i class="bi bi-plus-square" @click="add(product.id)"></i>
+                </div>
+              </v-col>
+              <v-divider v-if="index < productList.length - 1" class="my-2" />
+            </v-row>
+          </v-col>
+        </v-row>
+      </div>
+      <div v-else class="d-flex flex-column custom-align-center">
+        <img src="@/assets/images/cart.png" width="50%" height="auto" alt="">
+        <h2 class="text-center mt-4">目前購物車沒有商品喔~~</h2>
+      </div>
     </v-container>
   </div>
 </template>
@@ -54,19 +60,19 @@ const productList = ref([
 ]);
 
 function add(productId) {
-    for (const element of productList.value) {
-        if (productId == element.id) {
-            element.quantity++
-            break
-        }
+    const index = this.productList.findIndex(item => item.id === productId);
+    if (index !== -1) {
+        this.productList[index].quantity++;
     }
 }
 
 function minus(productId) {
-    for (const element of productList.value) {
-        if (productId == element.id && element.quantity > 1) {
-            element.quantity--
-            break
+    const index = this.productList.findIndex(item => item.id === productId);
+    if (index !== -1) {
+        if (this.productList[index].quantity > 1) {
+            this.productList[index].quantity--;
+        } else {
+            this.productList.splice(index, 1);
         }
     }
 }
